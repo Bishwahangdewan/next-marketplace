f8f5bcd9b290aa19db05e9bf1aff067ce61f4c0b
Bishwahang Dewan <bishwahangdewan123@gmail.com>
2022-09-06 18:17:41 +0530

    Scripts

---
+++ public/scripts/gtag.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+
+  gtag('config', 'G-WRF6CYGYJD');
+  gtag('config', 'AW-357026519');
+  gtag('config', 'G-VB9V0NB12R');
